1fd1d6e8cefcb15280ab99056ba62b209f91d557
HalfCoke <halfcokey@gmail.com>
2021-03-02 13:29:40 +0800

    [OK]<修改>(前端):格式化文档，增加BackTop按钮

--- searchwebui/src/App.vue
+++ searchwebui/src/App.vue
@@ -1,14 +1,31 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+	<div id="app">
+		<el-backtop :bottom="100">
+			<div class="back_top">
+				UP
+			</div>
+		</el-backtop>
+		<router-view/>
+	</div>
 </template>
 
 <style>
+#app {
+	height: 100%;
+}
 
+.back_top {
+	height: 100%;
+	width: 100%;
+	background-color: #f2f5f6;
+	box-shadow: 0 0 6px rgba(0, 0, 0, .12);
+	text-align: center;
+	line-height: 40px;
+	color: #1989fa;
+}
 </style>
 <script>
 export default {
-  name: 'App'
+	name: 'App'
 }
 </script>
--- searchwebui/src/views/Home/Home.vue
+++ searchwebui/src/views/Home/Home.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>
-    <el-container>
-      <el-header class="header">
-        <navbar/>
-      </el-header>
-    </el-container>
-    <main_intro/>
-    <main_content/>
-  </div>
+	<div>
+		<el-container>
+			<el-header class="header">
+				<navbar/>
+			</el-header>
+		</el-container>
+		<main_intro/>
+		<main_content/>
+	</div>
 </template>
 
 <script>
@@ -16,13 +16,10 @@ import Main_intro from "@/components/main_intro/main_intro";
 import Main_content from "@/components/main_content/main_content";
 
 export default {
-  name: "Home",
-  components: { Main_content, Main_intro, Navbar }
+	name: "Home",
+	components: { Main_content, Main_intro, Navbar }
 }
 </script>
 
 <style scoped>
-.main {
-  background-color: red;
-}
 </style>
